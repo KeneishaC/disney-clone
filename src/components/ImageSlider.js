6ea1761ac3fffd8890b44fa1e34e6d459322f5b7
keneishac <keneishacorbett@gmail.com>
2021-05-17 13:46:55 -0700

    completes slider and adds slider images

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -8,14 +8,38 @@ import "slick-carousel/slick/slick-theme.css";
 
 const Carousel = styled(Slider) `
     margin-top: 20px;
+    
+    ul li button {
+        &:before {
+            font-size: 10px;
+            color: rgba(150, 158, 171);
+        }
+    }
 
+    li.slcik-active button:before {
+        color: white;
+    }
+
+    .slcik-list {
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+    }
 `
 const Wrap = styled.div `
     img {
+        border: 4px solid transparent;
         border-radius: 4px;
         width: 100%;
         height: 100%;
         box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px - 10px, rgb( 0 0 0 / 73%) 0px 16px 10px - 10px;
+        transition-duration: 300ms;
+
+        &:hover {
+            border: 4px solid rgba(249, 249, 0.8);
+        }
     }
 `
 function ImageSlider() {
@@ -35,6 +59,12 @@ function ImageSlider() {
             </Wrap>
             <Wrap>
                 <img src="/images/slider-badag.jpg"/>
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-scale.jpg"/>
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-scales.jpg"/>
             </Wrap>
         </Carousel>
     )
