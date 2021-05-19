8746e28a3f9d862d83b0c3c601e56aa59359f46e
keneishac <keneishacorbett@gmail.com>
2021-05-18 17:57:19 -0700

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// import { Counter } from './features/counter/Counter';
 import Header from './components/Header'
 import Home from './components/Home'
+import Detail from './components/Detail'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail">
+              <Detail />
+          </Route>
+          <Route path="/">
+              <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
