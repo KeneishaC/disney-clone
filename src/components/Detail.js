f4d12f44be1b1d949dacc2afc3be5530f32c2876
keneishac <keneishacorbett@gmail.com>
2021-07-03 09:55:43 -0700

    updates made to useState

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { useParams } from "react-router-dom"
+import db from "../firebase"
+
+
 
 const Container = styled.div `
     min-height: calc(100vh - 70px);
@@ -101,13 +105,33 @@ const Description = styled.div `
 
 
 function Detail() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState({})
+
+    useEffect(() => {
+        db.collection("movies")
+        .doc(id)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                //save movie data
+               setMovie(doc.data()); 
+            } else {
+                //redirect to homepage
+
+            }
+        })
+    }, [id])
+
+    console.log("Movie is", movie)
+
     return (
         <Container>
             <Background>
-                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg"/>
+                <img src={movie.backgroundImg} />
             </Background>
             <ImageTitle>
-                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78"/>
+                <img src={movie.titleImg} />
             </ImageTitle>
             <Controls>
                 <PlayButton>
@@ -126,11 +150,10 @@ function Detail() {
                 </GroupWatchButton>
             </Controls>
             <Subtitle>
-                2018 • 7m • Family, Fantasy, Kids, Animation
+                {movie.subTitle}
             </Subtitle>
             <Description>
-                A Chinese mom who’s sad when her grown son leaves home gets another chance at motherhood when one of her dumplings springs to life. 
-                But she finds that nothing stays cute and small forever.
+                {movie.description}
             </Description>
         </Container>
     )
