9ea11668160c12d4036194bb21f9f742546212d1
keneishac <keneishacorbett@gmail.com>
2021-07-03 11:46:16 -0700

    adds user silce for user login and signout

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -29,4 +29,4 @@ export const selectUserName = (state) => state.user.name
 export const selectUserEmail = (state) => state.user.email
 export const selectUserPhoto = (state) => state.user.photo
 
-export default userSlice.reducer
+export default userSlice.reducer  
